12922826181406da524754347cac546e14c750e9
nik <nikita.nodejs@gmail.com>
2023-10-27 21:39:47 +0200

    add google authentication button

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,9 +1,35 @@
+import { Head } from "$fresh/runtime.ts";
 import { defineRoute } from "$fresh/src/server/defines.ts";
+import { CONFIG } from "../src/config.ts";
 
 export default defineRoute((req) => {
   return (
-    <div>
-      Hi there!
-    </div>
+    <>
+      <Head>
+        <script src="https://accounts.google.com/gsi/client" async></script>
+      </Head>
+      <div>
+        <div
+          id="g_id_onload"
+          data-client_id={CONFIG.GOOGLE_AUTH_CLIENT_ID}
+          data-context="use"
+          data-ux_mode="redirect"
+          data-login_uri={`${CONFIG.API_URL}/auth`}
+          data-itp_support="true"
+        >
+        </div>
+        <div
+          class="g_id_signin"
+          data-type="standard"
+          data-shape="pill"
+          data-theme="filled_black"
+          data-text="continue_with"
+          data-size="large"
+          data-locale="en-US"
+          data-logo_alignment="left"
+        >
+        </div>
+      </div>
+    </>
   );
 });
---
+++ src/config.ts
@@ -0,0 +1,30 @@
+import { load } from "$std/dotenv/mod.ts";
+
+const envPath = `${Deno.cwd()}/.env`;
+
+await load({ envPath, export: true });
+
+const CONFIG_KEYS = [
+  "GOOGLE_AUTH_CLIENT_ID",
+  "API_URL",
+] as const;
+
+export const CONFIG = CONFIG_KEYS.reduce((conf, key) => {
+  Object.assign(conf, { [key]: getValue(key) });
+
+  return conf;
+}, {} as Record<typeof CONFIG_KEYS[number], string>);
+
+function getValue(envName: string, throwError = false) {
+  const v = Deno.env.get(envName);
+
+  console.log(v);
+
+  if (v) return v;
+
+  const errorMessage = `${envName} is missing!`;
+
+  if (throwError) throw new Error(errorMessage);
+
+  return errorMessage;
+}
