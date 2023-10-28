cddb941adfbd7c26fe41cd44bb86db9b4700148b
nik <nikita.nodejs@gmail.com>
2023-10-28 23:37:29 +0200

    fix

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -2,7 +2,7 @@ import { MiddlewareHandler } from "$fresh/server.ts";
 import { getCookies } from "$std/http/cookie.ts";
 
 const REDIRECT_GUEST_TO_PUBLIC: MiddlewareHandler = (req, ctx) => {
-  if (ctx.destination !== "route" || /public/.test(req.url)) {
+  if (ctx.destination !== "route" || new URL(req.url).pathname.startsWith('/public')) {
     return ctx.next();
   }
 
