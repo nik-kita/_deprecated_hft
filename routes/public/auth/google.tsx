d2c58d04cf2287bdb594a24fe94626c1aa22a866
nik <nikita.nodejs@gmail.com>
2023-10-28 23:09:55 +0200

    fix: revert to previous, add headers to response

--- routes/public/auth/google.tsx
+++ routes/public/auth/google.tsx
@@ -5,12 +5,10 @@ export const handler: Handlers = {
   async POST(req, ctx) {
     ctx.state.gData = await req.formData();
 
-    console.log("formData", ctx.state.gData);
-
     const headers = new Headers();
 
     setCookie(headers, { name: "auth", value: "login" });
 
-    return new Response("Ok");
+    return new Response(null, { status: 204, headers });
   },
 };
